db2b8738e8e0cb3ce840f97c991327d14d360f0c
AhmedAWM <a.ahmed.aw@gmail.com>
2024-12-05 14:47:02 +0300

    Project 1

---
+++ js/data.js
@@ -0,0 +1,55 @@
+// Tries
+const tries = [3, 5, 7, 9, 11];
+
+// Game modes
+const easy = [
+    {type: 'animals', word: 'cat'},
+    {type: 'animals', word: 'bear'},
+    {type: 'animals', word: 'duck'},
+    {type: 'animals', word: 'chicken'},
+    {type: 'animals', word: 'lion'},
+    {type: 'food', word: 'apple'},
+    {type: 'food', word: 'bread'},
+    {type: 'food', word: 'cheese'},
+    {type: 'food', word: 'juice'},
+    {type: 'food', word: 'burger'},
+    {type: 'sports', word: 'football'},
+    {type: 'sports', word: 'basketball'},
+    {type: 'sports', word: 'volleyball'},
+    {type: 'sports', word: 'swimming'},
+    {type: 'sports', word: 'walking'}
+];
+const normal = [
+    {type: 'animals', word: 'elephant'},
+    {type: 'animals', word: 'kangaroo'},
+    {type: 'animals', word: 'giraffe'},
+    {type: 'animals', word: 'dolphine'},
+    {type: 'animals', word: 'gorilla'},
+    {type: 'food', word: 'chocolate'},
+    {type: 'food', word: 'pasta'},
+    {type: 'food', word: 'sausage'},
+    {type: 'food', word: 'tuna'},
+    {type: 'food', word: 'yogurt'},
+    {type: 'sports', word: 'biking'},
+    {type: 'sports', word: 'diving'},
+    {type: 'sports', word: 'handball'},
+    {type: 'sports', word: 'racing'},
+    {type: 'sports', word: 'skiing'}
+];
+const hard = [
+    {type: 'animals', word: 'squirrel'},
+    {type: 'animals', word: 'falmingo'},
+    {type: 'animals', word: 'chrocodile'},
+    {type: 'animals', word: 'hedgehog'},
+    {type: 'animals', word: 'starfish'},
+    {type: 'food', word: 'eggplant'},
+    {type: 'food', word: 'pumpkin'},
+    {type: 'food', word: 'walnut'},
+    {type: 'food', word: 'blueberry'},
+    {type: 'food', word: 'grapefruit'},
+    {type: 'sports', word: 'taekowndo'},
+    {type: 'sports', word: 'fencing'},
+    {type: 'sports', word: 'hockey'},
+    {type: 'sports', word: 'rafting'},
+    {type: 'sports', word: 'windsurfing'}
+];
